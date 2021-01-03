99e7fd92e7ff85e883e703b525913b52c5896d09
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-01-03 14:34:56 +0100

    Add To Cart Functionality

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -19,7 +19,7 @@ const routes = [
 const routesName = {
   homescreen: '/',
   productscreen: '/product/:id',
-  cartscreen: '/cart/:id?',
+  cartscreen: '/cart',
 };
 
 export { routes, routesName };
