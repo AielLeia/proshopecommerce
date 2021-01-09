feab09255c6dbe5d7644fd67782dc12a4bcf4169
isamelmohamedbouh <ismaelmedbouh@outlook.fr>
2021-01-09 16:03:03 +0100

    Checkout Steps Component

--- frontend/src/screens/ShippingScreen.jsx
+++ frontend/src/screens/ShippingScreen.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { saveShippingAddress } from '../actions';
-import { FormContainer } from '../components';
+import { FormContainer, CheckoutSteps } from '../components';
 
 const ShippingScreen = ({ history }) => {
   const cart = useSelector((state) => state.cart);
@@ -27,6 +27,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId='address'>
